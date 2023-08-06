92d53ed925dc4c76a574ca4782c4d1054150fa96
Mrinal Haque <mrinalhaque99@gmail.com>
2023-08-06 15:32:30 +0600

    Sample task Remove slug from product url

---
+++ inc/class-slug.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Slug class
+ * This do filter on 'post-type_link' hook
+ * This do action on 'pre_get_posts' and 'admin_init' hook
+ * 
+ * @package remove-slug-wc
+ */
+
+class Slug {
+    public function __construct() {
+        add_action( 'pre_get_posts', [ $this, 'rsw_change_slug_structure' ], 99 );
+        add_action( 'admin_init', [ $this, 'rsw_change_pretty_permalink' ] );
+
+        add_filter( 'post_type_link', [ $this, 'rsw_remove_slug' ], 10, 3 );
+        
+    }
+
+    function rsw_remove_slug( $post_link, $post, $leavename ) {
+        if ( 'product' != $post->post_type || 'publish' != $post->post_status ) {
+            return $post_link;
+        }
+        $woocommerce_base_slug = get_option('woocommerce_permalinks')['product_base'] . '/';
+        
+        $post_link = str_replace( $woocommerce_base_slug, '/', $post_link );
+        return $post_link;
+    }
+    
+    
+    function rsw_change_slug_structure( $query ) {
+        if ( ! $query->is_main_query() || 2 != count( $query->query ) || ! isset( $query->query['page'] ) ) {
+            return;
+        }
+        $post_types = get_post_types( array( 'public' => true ) );
+        if ( ! empty( $query->query['name'] ) ) {
+            $query->set( 'post_type', $post_types );
+        } elseif ( ! empty( $query->query['pagename'] ) && false === strpos( $query->query['pagename'], '/' ) ) {
+            $query->set( 'post_type', $post_types );
+            $query->set( 'name', $query->query['pagename'] );
+        }
+    }
+    
+    
+    
+    function rsw_change_pretty_permalink() {
+        global $wp_rewrite; 
+    
+        $wp_rewrite->set_permalink_structure('/%postname%/'); 
+    
+        update_option( "rewrite_rules", FALSE ); 
+    
+        $wp_rewrite->flush_rules( true );
+    }
+}
+
---
+++ remove-slug-wc.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name: Remove WooCommerce Base
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	die();
+}
+
+include_once plugin_dir_path( __FILE__ ) . 'inc/class-slug.php';
+
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    new Slug();
+}
+
